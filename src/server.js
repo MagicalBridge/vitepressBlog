ef3c10f9f4513ad3c7043bb4c48f660ab39a6db0
褚鹏飞 <540788769@qq.com>
2022-11-19 00:34:30 +0800

    feat: 只简单启动服务

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const path = require('path');
 
 const startServer = async () => {
   const app = express();
   app.all('/healthcheck', function (req, res) {
     res.send('OK');
   });
-  app.use(express.static(path.join(__dirname, '../docs/.vitepress/dist')));
   // 监听端口号
   app.listen(8080, () => {
     console.log('服务已启动, 端口8080');
